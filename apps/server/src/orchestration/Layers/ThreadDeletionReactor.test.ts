tmacc/agentdynamo

fix(web): allow deleting non-empty projects from the warning toast (#1264)
Co-authored-by: Julius Marminge <julius0216@outlook.com>
Co-authored-by: codex <codex@users.noreply.github.com>

apps/server/src/orchestration/Layers/ThreadDeletionReactor.test.ts
@@ -1,0 +1,36 @@
+import { ThreadId } from "@t3tools/contracts";
+import { Cause, Effect, Exit } from "effect";
+import { describe, expect, it } from "vitest";
+
+import { logCleanupCauseUnlessInterrupted } from "./ThreadDeletionReactor.ts";
+
+describe("logCleanupCauseUnlessInterrupted", () => {
+  const threadId = ThreadId.make("thread-deletion-reactor-test");
+
+  it("swallows ordinary cleanup failures", async () => {
+    const exit = await Effect.runPromiseExit(
+      logCleanupCauseUnlessInterrupted({
+        effect: Effect.fail("cleanup failed"),
+        message: "thread deletion cleanup skipped provider session stop",
+        threadId,
+      }),
+    );
+
+    expect(Exit.isSuccess(exit)).toBe(true);
+  });
+
+  it("preserves interrupt causes", async () => {
+    const exit = await Effect.runPromiseExit(
+      logCleanupCauseUnlessInterrupted({
+        effect: Effect.interrupt,
+        message: "thread deletion cleanup skipped provider session stop",
+        threadId,
+      }),
+    );
+
+    expect(Exit.isFailure(exit)).toBe(true);
+    if (Exit.isFailure(exit)) {
+      expect(Cause.hasInterruptsOnly(exit.cause)).toBe(true);
+    }
+  });
+});

apps/server/src/orchestration/Layers/ThreadDeletionReactor.ts
@@ -1,0 +1,96 @@
+import type { OrchestrationEvent } from "@t3tools/contracts";
+import { makeDrainableWorker } from "@t3tools/shared/DrainableWorker";
+import { Cause, Effect, Layer, Stream } from "effect";
+
+import { ProviderService } from "../../provider/Services/ProviderService.ts";
+import { TerminalManager } from "../../terminal/Services/Manager.ts";
+import { OrchestrationEngineService } from "../Services/OrchestrationEngine.ts";
+import {
+  ThreadDeletionReactor,
+  type ThreadDeletionReactorShape,
+} from "../Services/ThreadDeletionReactor.ts";
+
+type ThreadDeletedEvent = Extract<OrchestrationEvent, { type: "thread.deleted" }>;
+
+export const logCleanupCauseUnlessInterrupted = <R, E>({
+  effect,
+  message,
+  threadId,
+}: {
+  readonly effect: Effect.Effect<void, E, R>;
+  readonly message: string;
+  readonly threadId: ThreadDeletedEvent["payload"]["threadId"];
+}): Effect.Effect<void, E, R> =>
+  effect.pipe(
+    Effect.catchCause((cause) => {
+      if (Cause.hasInterruptsOnly(cause)) {
+        return Effect.failCause(cause);
+      }
+      return Effect.logDebug(message, {
+        threadId,
+        cause: Cause.pretty(cause),
+      });
+    }),
+  );
+
+const make = Effect.gen(function* () {
+  const orchestrationEngine = yield* OrchestrationEngineService;
+  const providerService = yield* ProviderService;
+  const terminalManager = yield* TerminalManager;
+
+  const stopProviderSession = (threadId: ThreadDeletedEvent["payload"]["threadId"]) =>
+    logCleanupCauseUnlessInterrupted({
+      effect: providerService.stopSession({ threadId }),
+      message: "thread deletion cleanup skipped provider session stop",
+      threadId,
+    });
+
+  const closeThreadTerminals = (threadId: ThreadDeletedEvent["payload"]["threadId"]) =>
+    logCleanupCauseUnlessInterrupted({
+      effect: terminalManager.close({ threadId, deleteHistory: true }),
+      message: "thread deletion cleanup skipped terminal close",
+      threadId,
+    });
+
+  const processThreadDeleted = Effect.fn("processThreadDeleted")(function* (
+    event: ThreadDeletedEvent,
+  ) {
+    const { threadId } = event.payload;
+    yield* stopProviderSession(threadId);
+    yield* closeThreadTerminals(threadId);
+  });
+
+  const processThreadDeletedSafely = (event: ThreadDeletedEvent) =>
+    processThreadDeleted(event).pipe(
+      Effect.catchCause((cause) => {
+        if (Cause.hasInterruptsOnly(cause)) {
+          return Effect.failCause(cause);
+        }
+        return Effect.logWarning("thread deletion reactor failed to process event", {
+          eventType: event.type,
+          threadId: event.payload.threadId,
+          cause: Cause.pretty(cause),
+        });
+      }),
+    );
+
+  const worker = yield* makeDrainableWorker(processThreadDeletedSafely);
+
+  const start: ThreadDeletionReactorShape["start"] = Effect.fn("start")(function* () {
+    yield* Effect.forkScoped(
+      Stream.runForEach(orchestrationEngine.streamDomainEvents, (event) => {
+        if (event.type !== "thread.deleted") {
+          return Effect.void;
+        }
+        return worker.enqueue(event);
+      }),
+    );
+  });
+
+  return {
+    start,
+    drain: worker.drain,
+  } satisfies ThreadDeletionReactorShape;
+});
+
+export const ThreadDeletionReactorLive = Layer.effect(ThreadDeletionReactor, make);

apps/server/src/orchestration/decider.delete.test.ts
@@ -1,0 +1,226 @@
+import {
+  CommandId,
+  DEFAULT_PROVIDER_INTERACTION_MODE,
+  EventId,
+  ProjectId,
+  ThreadId,
+  type OrchestrationCommand,
+  type OrchestrationEvent,
+  type OrchestrationReadModel,
+} from "@t3tools/contracts";
+import { Effect } from "effect";
+import { describe, expect, it } from "vitest";
+
+import { decideOrchestrationCommand } from "./decider.ts";
+import { createEmptyReadModel, projectEvent } from "./projector.ts";
+
+const asCommandId = (value: string): CommandId => CommandId.make(value);
+const asEventId = (value: string): EventId => EventId.make(value);
+const asProjectId = (value: string): ProjectId => ProjectId.make(value);
+const asThreadId = (value: string): ThreadId => ThreadId.make(value);
+
+async function seedReadModel(): Promise<OrchestrationReadModel> {
+  const now = new Date().toISOString();
+  const initial = createEmptyReadModel(now);
+  const withProject = await Effect.runPromise(
+    projectEvent(initial, {
+      sequence: 1,
+      eventId: asEventId("evt-project-create"),
+      aggregateKind: "project",
+      aggregateId: asProjectId("project-delete"),
+      type: "project.created",
+      occurredAt: now,
+      commandId: asCommandId("cmd-project-create"),
+      causationEventId: null,
+      correlationId: asCommandId("cmd-project-create"),
+      metadata: {},
+      payload: {
+        projectId: asProjectId("project-delete"),
+        title: "Project Delete",
+        workspaceRoot: "/tmp/project-delete",
+        defaultModelSelection: null,
+        scripts: [],
+        createdAt: now,
+        updatedAt: now,
+      },
+    }),
+  );
+
+  const withFirstThread = await Effect.runPromise(
+    projectEvent(withProject, {
+      sequence: 2,
+      eventId: asEventId("evt-thread-create-1"),
+      aggregateKind: "thread",
+      aggregateId: asThreadId("thread-delete-1"),
+      type: "thread.created",
+      occurredAt: now,
+      commandId: asCommandId("cmd-thread-create-1"),
+      causationEventId: null,
+      correlationId: asCommandId("cmd-thread-create-1"),
+      metadata: {},
+      payload: {
+        threadId: asThreadId("thread-delete-1"),
+        projectId: asProjectId("project-delete"),
+        title: "Thread Delete 1",
+        modelSelection: {
+          provider: "codex",
+          model: "gpt-5-codex",
+        },
+        interactionMode: DEFAULT_PROVIDER_INTERACTION_MODE,
+        runtimeMode: "approval-required",
+        branch: null,
+        worktreePath: null,
+        createdAt: now,
+        updatedAt: now,
+      },
+    }),
+  );
+
+  return Effect.runPromise(
+    projectEvent(withFirstThread, {
+      sequence: 3,
+      eventId: asEventId("evt-thread-create-2"),
+      aggregateKind: "thread",
+      aggregateId: asThreadId("thread-delete-2"),
+      type: "thread.created",
+      occurredAt: now,
+      commandId: asCommandId("cmd-thread-create-2"),
+      causationEventId: null,
+      correlationId: asCommandId("cmd-thread-create-2"),
+      metadata: {},
+      payload: {
+        threadId: asThreadId("thread-delete-2"),
+        projectId: asProjectId("project-delete"),
+        title: "Thread Delete 2",
+        modelSelection: {
+          provider: "codex",
+          model: "gpt-5-codex",
+        },
+        interactionMode: DEFAULT_PROVIDER_INTERACTION_MODE,
+        runtimeMode: "approval-required",
+        branch: null,
+        worktreePath: null,
+        createdAt: now,
+        updatedAt: now,
+      },
+    }),
+  );
+}
+
+type PlannedEvent = Omit<OrchestrationEvent, "sequence">;
+
+function normalizeDeleteEvent(event: PlannedEvent | ReadonlyArray<PlannedEvent>) {
+  const events = Array.isArray(event) ? event : [event];
+  return events.map((entry) => {
+    switch (entry.type) {
+      case "thread.deleted":
+        return {
+          type: entry.type,
+          aggregateKind: entry.aggregateKind,
+          aggregateId: entry.aggregateId,
+          commandId: entry.commandId,
+          correlationId: entry.correlationId,
+          payload: {
+            threadId: entry.payload.threadId,
+          },
+        };
+      case "project.deleted":
+        return {
+          type: entry.type,
+          aggregateKind: entry.aggregateKind,
+          aggregateId: entry.aggregateId,
+          commandId: entry.commandId,
+          correlationId: entry.correlationId,
+          payload: {
+            projectId: entry.payload.projectId,
+          },
+        };
+      default:
+        return entry;
+    }
+  });
+}
+
+describe("decider deletion flows", () => {
+  it("rejects deleting a non-empty project without force", async () => {
+    const readModel = await seedReadModel();
+
+    await expect(
+      Effect.runPromise(
+        decideOrchestrationCommand({
+          command: {
+            type: "project.delete",
+            commandId: asCommandId("cmd-project-delete-no-force"),
+            projectId: asProjectId("project-delete"),
+          },
+          readModel,
+        }),
+      ),
+    ).rejects.toThrow("cannot be deleted without force=true");
+  });
+
+  it("reuses thread.delete semantics when force-deleting a non-empty project", async () => {
+    const readModel = await seedReadModel();
+    const projectDeleteCommand: Extract<OrchestrationCommand, { type: "project.delete" }> = {
+      type: "project.delete",
+      commandId: asCommandId("cmd-project-delete-force"),
+      projectId: asProjectId("project-delete"),
+      force: true,
+    };
+
+    const forcedResult = await Effect.runPromise(
+      decideOrchestrationCommand({
+        command: projectDeleteCommand,
+        readModel,
+      }),
+    );
+    const forcedEvents = Array.isArray(forcedResult) ? forcedResult : [forcedResult];
+
+    expect(forcedEvents.map((event) => event.type)).toEqual([
+      "thread.deleted",
+      "thread.deleted",
+      "project.deleted",
+    ]);
+
+    let sequentialReadModel = readModel;
+    let nextSequence = readModel.snapshotSequence;
+    const sequentialEvents: PlannedEvent[] = [];
+    for (const nextCommand of [
+      {
+        type: "thread.delete",
+        commandId: projectDeleteCommand.commandId,
+        threadId: asThreadId("thread-delete-1"),
+      },
+      {
+        type: "thread.delete",
+        commandId: projectDeleteCommand.commandId,
+        threadId: asThreadId("thread-delete-2"),
+      },
+      {
+        type: "project.delete",
+        commandId: projectDeleteCommand.commandId,
+        projectId: asProjectId("project-delete"),
+      },
+    ] satisfies ReadonlyArray<OrchestrationCommand>) {
+      const decided = await Effect.runPromise(
+        decideOrchestrationCommand({
+          command: nextCommand,
+          readModel: sequentialReadModel,
+        }),
+      );
+      const nextEvents = Array.isArray(decided) ? decided : [decided];
+      sequentialEvents.push(...nextEvents);
+      for (const nextEvent of nextEvents) {
+        nextSequence += 1;
+        sequentialReadModel = await Effect.runPromise(
+          projectEvent(sequentialReadModel, {
+            ...nextEvent,
+            sequence: nextSequence,
+          }),
+        );
+      }
+    }
+
+    expect(normalizeDeleteEvent(forcedResult)).toEqual(normalizeDeleteEvent(sequentialEvents));
+  });
+});

me-safty/dxcode

apps/server/src/orchestration/Layers/ThreadDeletionReactor.test.ts
@@ -1,0 +1,36 @@
+import { ThreadId } from "@marcode/contracts";
+import { Cause, Effect, Exit } from "effect";
+import { describe, expect, it } from "vitest";
+
+import { logCleanupCauseUnlessInterrupted } from "./ThreadDeletionReactor.ts";
+
+describe("logCleanupCauseUnlessInterrupted", () => {
+  const threadId = ThreadId.make("thread-deletion-reactor-test");
+
+  it("swallows ordinary cleanup failures", async () => {
+    const exit = await Effect.runPromiseExit(
+      logCleanupCauseUnlessInterrupted({
+        effect: Effect.fail("cleanup failed"),
+        message: "thread deletion cleanup skipped provider session stop",
+        threadId,
+      }),
+    );
+
+    expect(Exit.isSuccess(exit)).toBe(true);
+  });
+
+  it("preserves interrupt causes", async () => {
+    const exit = await Effect.runPromiseExit(
+      logCleanupCauseUnlessInterrupted({
+        effect: Effect.interrupt,
+        message: "thread deletion cleanup skipped provider session stop",
+        threadId,
+      }),
+    );
+
+    expect(Exit.isFailure(exit)).toBe(true);
+    if (Exit.isFailure(exit)) {
+      expect(Cause.hasInterruptsOnly(exit.cause)).toBe(true);
+    }
+  });
+});

apps/server/src/orchestration/Layers/ThreadDeletionReactor.ts
@@ -1,0 +1,96 @@
+import type { OrchestrationEvent } from "@marcode/contracts";
+import { makeDrainableWorker } from "@marcode/shared/DrainableWorker";
+import { Cause, Effect, Layer, Stream } from "effect";
+
+import { ProviderService } from "../../provider/Services/ProviderService.ts";
+import { TerminalManager } from "../../terminal/Services/Manager.ts";
+import { OrchestrationEngineService } from "../Services/OrchestrationEngine.ts";
+import {
+  ThreadDeletionReactor,
+  type ThreadDeletionReactorShape,
+} from "../Services/ThreadDeletionReactor.ts";
+
+type ThreadDeletedEvent = Extract<OrchestrationEvent, { type: "thread.deleted" }>;
+
+export const logCleanupCauseUnlessInterrupted = <R, E>({
+  effect,
+  message,
+  threadId,
+}: {
+  readonly effect: Effect.Effect<void, E, R>;
+  readonly message: string;
+  readonly threadId: ThreadDeletedEvent["payload"]["threadId"];
+}): Effect.Effect<void, E, R> =>
+  effect.pipe(
+    Effect.catchCause((cause) => {
+      if (Cause.hasInterruptsOnly(cause)) {
+        return Effect.failCause(cause);
+      }
+      return Effect.logDebug(message, {
+        threadId,
+        cause: Cause.pretty(cause),
+      });
+    }),
+  );
+
+const make = Effect.gen(function* () {
+  const orchestrationEngine = yield* OrchestrationEngineService;
+  const providerService = yield* ProviderService;
+  const terminalManager = yield* TerminalManager;
+
+  const stopProviderSession = (threadId: ThreadDeletedEvent["payload"]["threadId"]) =>
+    logCleanupCauseUnlessInterrupted({
+      effect: providerService.stopSession({ threadId }),
+      message: "thread deletion cleanup skipped provider session stop",
+      threadId,
+    });
+
+  const closeThreadTerminals = (threadId: ThreadDeletedEvent["payload"]["threadId"]) =>
+    logCleanupCauseUnlessInterrupted({
+      effect: terminalManager.close({ threadId, deleteHistory: true }),
+      message: "thread deletion cleanup skipped terminal close",
+      threadId,
+    });
+
+  const processThreadDeleted = Effect.fn("processThreadDeleted")(function* (
+    event: ThreadDeletedEvent,
+  ) {
+    const { threadId } = event.payload;
+    yield* stopProviderSession(threadId);
+    yield* closeThreadTerminals(threadId);
+  });
+
+  const processThreadDeletedSafely = (event: ThreadDeletedEvent) =>
+    processThreadDeleted(event).pipe(
+      Effect.catchCause((cause) => {
+        if (Cause.hasInterruptsOnly(cause)) {
+          return Effect.failCause(cause);
+        }
+        return Effect.logWarning("thread deletion reactor failed to process event", {
+          eventType: event.type,
+          threadId: event.payload.threadId,
+          cause: Cause.pretty(cause),
+        });
+      }),
+    );
+
+  const worker = yield* makeDrainableWorker(processThreadDeletedSafely);
+
+  const start: ThreadDeletionReactorShape["start"] = Effect.fn("start")(function* () {
+    yield* Effect.forkScoped(
+      Stream.runForEach(orchestrationEngine.streamDomainEvents, (event) => {
+        if (event.type !== "thread.deleted") {
+          return Effect.void;
+        }
+        return worker.enqueue(event);
+      }),
+    );
+  });
+
+  return {
+    start,
+    drain: worker.drain,
+  } satisfies ThreadDeletionReactorShape;
+});
+
+export const ThreadDeletionReactorLive = Layer.effect(ThreadDeletionReactor, make);

apps/server/src/orchestration/decider.delete.test.ts
@@ -1,0 +1,226 @@
+import {
+  CommandId,
+  DEFAULT_PROVIDER_INTERACTION_MODE,
+  EventId,
+  ProjectId,
+  ThreadId,
+  type OrchestrationCommand,
+  type OrchestrationEvent,
+  type OrchestrationReadModel,
+} from "@marcode/contracts";
+import { Effect } from "effect";
+import { describe, expect, it } from "vitest";
+
+import { decideOrchestrationCommand } from "./decider.ts";
+import { createEmptyReadModel, projectEvent } from "./projector.ts";
+
+const asCommandId = (value: string): CommandId => CommandId.make(value);
+const asEventId = (value: string): EventId => EventId.make(value);
+const asProjectId = (value: string): ProjectId => ProjectId.make(value);
+const asThreadId = (value: string): ThreadId => ThreadId.make(value);
+
+async function seedReadModel(): Promise<OrchestrationReadModel> {
+  const now = new Date().toISOString();
+  const initial = createEmptyReadModel(now);
+  const withProject = await Effect.runPromise(
+    projectEvent(initial, {
+      sequence: 1,
+      eventId: asEventId("evt-project-create"),
+      aggregateKind: "project",
+      aggregateId: asProjectId("project-delete"),
+      type: "project.created",
+      occurredAt: now,
+      commandId: asCommandId("cmd-project-create"),
+      causationEventId: null,
+      correlationId: asCommandId("cmd-project-create"),
+      metadata: {},
+      payload: {
+        projectId: asProjectId("project-delete"),
+        title: "Project Delete",
+        workspaceRoot: "/tmp/project-delete",
+        defaultModelSelection: null,
+        scripts: [],
+        createdAt: now,
+        updatedAt: now,
+      },
+    }),
+  );
+
+  const withFirstThread = await Effect.runPromise(
+    projectEvent(withProject, {
+      sequence: 2,
+      eventId: asEventId("evt-thread-create-1"),
+      aggregateKind: "thread",
+      aggregateId: asThreadId("thread-delete-1"),
+      type: "thread.created",
+      occurredAt: now,
+      commandId: asCommandId("cmd-thread-create-1"),
+      causationEventId: null,
+      correlationId: asCommandId("cmd-thread-create-1"),
+      metadata: {},
+      payload: {
+        threadId: asThreadId("thread-delete-1"),
+        projectId: asProjectId("project-delete"),
+        title: "Thread Delete 1",
+        modelSelection: {
+          provider: "codex",
+          model: "gpt-5-codex",
+        },
+        interactionMode: DEFAULT_PROVIDER_INTERACTION_MODE,
+        runtimeMode: "approval-required",
+        branch: null,
+        worktreePath: null,
+        createdAt: now,
+        updatedAt: now,
+      },
+    }),
+  );
+
+  return Effect.runPromise(
+    projectEvent(withFirstThread, {
+      sequence: 3,
+      eventId: asEventId("evt-thread-create-2"),
+      aggregateKind: "thread",
+      aggregateId: asThreadId("thread-delete-2"),
+      type: "thread.created",
+      occurredAt: now,
+      commandId: asCommandId("cmd-thread-create-2"),
+      causationEventId: null,
+      correlationId: asCommandId("cmd-thread-create-2"),
+      metadata: {},
+      payload: {
+        threadId: asThreadId("thread-delete-2"),
+        projectId: asProjectId("project-delete"),
+        title: "Thread Delete 2",
+        modelSelection: {
+          provider: "codex",
+          model: "gpt-5-codex",
+        },
+        interactionMode: DEFAULT_PROVIDER_INTERACTION_MODE,
+        runtimeMode: "approval-required",
+        branch: null,
+        worktreePath: null,
+        createdAt: now,
+        updatedAt: now,
+      },
+    }),
+  );
+}
+
+type PlannedEvent = Omit<OrchestrationEvent, "sequence">;
+
+function normalizeDeleteEvent(event: PlannedEvent | ReadonlyArray<PlannedEvent>) {
+  const events = Array.isArray(event) ? event : [event];
+  return events.map((entry) => {
+    switch (entry.type) {
+      case "thread.deleted":
+        return {
+          type: entry.type,
+          aggregateKind: entry.aggregateKind,
+          aggregateId: entry.aggregateId,
+          commandId: entry.commandId,
+          correlationId: entry.correlationId,
+          payload: {
+            threadId: entry.payload.threadId,
+          },
+        };
+      case "project.deleted":
+        return {
+          type: entry.type,
+          aggregateKind: entry.aggregateKind,
+          aggregateId: entry.aggregateId,
+          commandId: entry.commandId,
+          correlationId: entry.correlationId,
+          payload: {
+            projectId: entry.payload.projectId,
+          },
+        };
+      default:
+        return entry;
+    }
+  });
+}
+
+describe("decider deletion flows", () => {
+  it("rejects deleting a non-empty project without force", async () => {
+    const readModel = await seedReadModel();
+
+    await expect(
+      Effect.runPromise(
+        decideOrchestrationCommand({
+          command: {
+            type: "project.delete",
+            commandId: asCommandId("cmd-project-delete-no-force"),
+            projectId: asProjectId("project-delete"),
+          },
+          readModel,
+        }),
+      ),
+    ).rejects.toThrow("cannot be deleted without force=true");
+  });
+
+  it("reuses thread.delete semantics when force-deleting a non-empty project", async () => {
+    const readModel = await seedReadModel();
+    const projectDeleteCommand: Extract<OrchestrationCommand, { type: "project.delete" }> = {
+      type: "project.delete",
+      commandId: asCommandId("cmd-project-delete-force"),
+      projectId: asProjectId("project-delete"),
+      force: true,
+    };
+
+    const forcedResult = await Effect.runPromise(
+      decideOrchestrationCommand({
+        command: projectDeleteCommand,
+        readModel,
+      }),
+    );
+    const forcedEvents = Array.isArray(forcedResult) ? forcedResult : [forcedResult];
+
+    expect(forcedEvents.map((event) => event.type)).toEqual([
+      "thread.deleted",
+      "thread.deleted",
+      "project.deleted",
+    ]);
+
+    let sequentialReadModel = readModel;
+    let nextSequence = readModel.snapshotSequence;
+    const sequentialEvents: PlannedEvent[] = [];
+    for (const nextCommand of [
+      {
+        type: "thread.delete",
+        commandId: projectDeleteCommand.commandId,
+        threadId: asThreadId("thread-delete-1"),
+      },
+      {
+        type: "thread.delete",
+        commandId: projectDeleteCommand.commandId,
+        threadId: asThreadId("thread-delete-2"),
+      },
+      {
+        type: "project.delete",
+        commandId: projectDeleteCommand.commandId,
+        projectId: asProjectId("project-delete"),
+      },
+    ] satisfies ReadonlyArray<OrchestrationCommand>) {
+      const decided = await Effect.runPromise(
+        decideOrchestrationCommand({
+          command: nextCommand,
+          readModel: sequentialReadModel,
+        }),
+      );
+      const nextEvents = Array.isArray(decided) ? decided : [decided];
+      sequentialEvents.push(...nextEvents);
+      for (const nextEvent of nextEvents) {
+        nextSequence += 1;
+        sequentialReadModel = await Effect.runPromise(
+          projectEvent(sequentialReadModel, {
+            ...nextEvent,
+            sequence: nextSequence,
+          }),
+        );
+      }
+    }
+
+    expect(normalizeDeleteEvent(forcedResult)).toEqual(normalizeDeleteEvent(sequentialEvents));
+  });
+});