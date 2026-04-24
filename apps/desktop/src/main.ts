tmacc/agentdynamo

chore(desktop): separate dev AppUserModelID on Windows (#1934)

apps/desktop/src/main.ts
@@ -100,7 +100,7 @@ const DESKTOP_SCHEME = "t3";
 const ROOT_DIR = Path.resolve(__dirname, "../../..");
 const isDevelopment = Boolean(process.env.VITE_DEV_SERVER_URL);
 const APP_DISPLAY_NAME = isDevelopment ? "T3 Code (Dev)" : "T3 Code (Alpha)";
-const APP_USER_MODEL_ID = "com.t3tools.t3code";
+const APP_USER_MODEL_ID = isDevelopment ? "com.t3tools.t3code.dev" : "com.t3tools.t3code";
 const LINUX_DESKTOP_ENTRY_NAME = isDevelopment ? "t3code-dev.desktop" : "t3code.desktop";
 const LINUX_WM_CLASS = isDevelopment ? "t3code-dev" : "t3code";
 const USER_DATA_DIR_NAME = isDevelopment ? "t3code-dev" : "t3code";

me-safty/dxcode

(cherry picked from commit f9372a4c3d7ecb33120922397c26d5cf0c143970)

apps/desktop/src/main.ts
@@ -111,7 +111,7 @@ const DESKTOP_SCHEME = "marcode";
 const ROOT_DIR = Path.resolve(__dirname, "../../..");
 const isDevelopment = Boolean(process.env.VITE_DEV_SERVER_URL);
 const APP_DISPLAY_NAME = isDevelopment ? "MarCode (Dev)" : "MarCode (Alpha)";
-const APP_USER_MODEL_ID = "com.marcode.marcode";
+const APP_USER_MODEL_ID = isDevelopment ? "com.marcode.marcode.dev" : "com.marcode.marcode";
 const LINUX_DESKTOP_ENTRY_NAME = isDevelopment ? "marcode-dev.desktop" : "marcode.desktop";
 const LINUX_WM_CLASS = isDevelopment ? "marcode-dev" : "marcode";
 const USER_DATA_DIR_NAME = isDevelopment ? "marcode-dev" : "marcode";