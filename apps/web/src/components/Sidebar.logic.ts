pingdotgg/t3code

Show provider icons for active sidebar threads

apps/web/src/components/Sidebar.logic.ts
@@ -1,4 +1,5 @@
 import * as React from "react";
+import type { ProviderKind } from "@t3tools/contracts";
 import type { SidebarProjectSortOrder, SidebarThreadSortOrder } from "@t3tools/contracts/settings";
 import {
   getThreadSortTimestamp,
@@ -36,6 +37,7 @@ export interface ThreadStatusPill {
   colorClass: string;
   dotClass: string;
   pulse: boolean;
+  workingProvider?: ProviderKind;
 }
 
 const THREAD_STATUS_PRIORITY: Record<ThreadStatusPill["label"], number> = {
@@ -355,6 +357,7 @@ export function resolveThreadStatusPill(input: {
       colorClass: "text-sky-600 dark:text-sky-300/80",
       dotClass: "bg-sky-500 dark:bg-sky-300/80",
       pulse: true,
+      workingProvider: thread.session.provider,
     };
   }
 
@@ -364,6 +367,7 @@ export function resolveThreadStatusPill(input: {
       colorClass: "text-sky-600 dark:text-sky-300/80",
       dotClass: "bg-sky-500 dark:bg-sky-300/80",
       pulse: true,
+      workingProvider: thread.session.provider,
     };
   }
 
@@ -387,6 +391,7 @@ export function resolveThreadStatusPill(input: {
       colorClass: "text-emerald-600 dark:text-emerald-300/90",
       dotClass: "bg-emerald-500 dark:bg-emerald-300/90",
       pulse: false,
+      ...(thread.session?.provider ? { workingProvider: thread.session.provider } : {}),
     };
   }
 

me-safty/dxcode

apps/web/src/components/Sidebar.logic.ts
@@ -1,4 +1,5 @@
 import * as React from "react";
+import type { ProviderDriverKind } from "@t3tools/contracts";
 import type { SidebarProjectSortOrder, SidebarThreadSortOrder } from "@t3tools/contracts/settings";
 import {
   getThreadSortTimestamp,
@@ -36,6 +37,7 @@ export interface ThreadStatusPill {
   colorClass: string;
   dotClass: string;
   pulse: boolean;
+  workingProvider?: ProviderDriverKind;
 }
 
 const THREAD_STATUS_PRIORITY: Record<ThreadStatusPill["label"], number> = {
@@ -364,6 +366,7 @@ export function resolveThreadStatusPill(input: {
       colorClass: "text-sky-600 dark:text-sky-300/80",
       dotClass: "bg-sky-500 dark:bg-sky-300/80",
       pulse: true,
+      workingProvider: thread.session.provider,
     };
   }
 
@@ -373,6 +376,7 @@ export function resolveThreadStatusPill(input: {
       colorClass: "text-sky-600 dark:text-sky-300/80",
       dotClass: "bg-sky-500 dark:bg-sky-300/80",
       pulse: true,
+      workingProvider: thread.session.provider,
     };
   }
 
@@ -396,6 +400,7 @@ export function resolveThreadStatusPill(input: {
       colorClass: "text-emerald-600 dark:text-emerald-300/90",
       dotClass: "bg-emerald-500 dark:bg-emerald-300/90",
       pulse: false,
+      ...(thread.session?.provider ? { workingProvider: thread.session.provider } : {}),
     };
   }
 